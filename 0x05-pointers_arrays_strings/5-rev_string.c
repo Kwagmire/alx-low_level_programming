3152f738829a638f0f046126d8789fa0966744b7
Kwagmire <kwagmire999@gmail.com>
2022-04-21 08:48:55 +0100

    strcpy

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,21 +8,21 @@
 
 void rev_string(char *s)
 {
+	char *rev;
 	int len = 0, i, j;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		len++;
 	}
-	char *reverse;
 
 	for (; len >= 1; len--)
 	{
-		reverse[i - (len - 1)] = s[len - 1];
+		rev[i - (len - 1)] = s[len - 1];
 	}
 
 	for (j = 0; j < i; j++)
 	{
-		s[j] = reverse[j];
+		s[j] = rev[j];
 	}
 }
