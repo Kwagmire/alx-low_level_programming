0a2ff116c113d1e223bdd3863b7a9d9903e8ee79
Kwagmire <kwagmire999@gmail.com>
2022-04-23 11:11:51 +0100

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,12 +10,14 @@
 
 int *_strcmp(char *s1, char *s2)
 {
-	int i;
+	int i, diff;
 
 	for (i = 0; s1[i] == s2[i]; i++)
 	{
 		if (s1[i] == '\0')
 			break;
 	}
-
-	return (s1[i] - s2[i]);
+	
+	diff = s1[i] - s2[i];
+	return (diff);
+}
