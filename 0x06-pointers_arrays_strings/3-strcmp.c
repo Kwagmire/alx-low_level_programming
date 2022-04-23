674430dc4d52a935ea094cfd0ec0ce8dcdde0dab
Kwagmire <kwagmire999@gmail.com>
2022-04-23 15:59:10 +0100

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,11 +10,16 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && *s2 && *s1 == *s2)
+	while (*s1)
 	{
+		if (*s1 != *s2)
+			break;
 		s1++;
 		s2++;
 	}
-	
-	return (*(unsigned char *)s1 - *(unsigned char *)s2);
+
+	if (!(*s1 && *s2))
+		return (0);
+	else
+		return (*(unsigned char *)s1 - *(unsigned char *)s2);
 }
