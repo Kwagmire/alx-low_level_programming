aae660b7401a46f97c4a937320be063598442a7d
Kwagmire <kwagmire999@gmail.com>
2022-04-24 12:28:23 +0100

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,7 +13,10 @@ char *cap_string(char *a)
 
 	while (a[i])
 	{
-		if (a[i] == ' ' || a[i] == '\t' || a[i] == '\n' || a[i] == ',')
+		if (i == 0 && a[i] >= 'a' && a[i] <= 'z')
+			a[i] -= 32;
+
+		else if (a[i] == ' ' || a[i] == '\t' || a[i] == '\n' || a[i] == ',')
 		{
 			if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
 				a[i + 1] -= 32;
