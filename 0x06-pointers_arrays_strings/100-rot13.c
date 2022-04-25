672a0a462450ecba3d8e1b5dffabe4bc45c88df1
Kwagmire <kwagmire999@gmail.com>
2022-04-25 10:03:20 +0100

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * rot13 - encode string using rot13
+ * @str: the string to be manipulated
+ *
+ * Return: resultant string
+ */
+
+char *rot13(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		while (
+				(str[i] >= 'a' &&
+				 str[i] <= 'z') ||
+				(str[i] >= 'A' &&
+				 str[i] <= 'Z')
+		      )
+		{
+			if (
+					(str[i] >= 'n' &&
+					 str[i] <= 'z') ||
+					(str[i] >= 'N' &&
+					 str[i] <= 'Z')
+			   )
+				str[i] = str[i] - 13;
+			else
+				str[i] = str[i] + 13;
+			i++;
+		}
+
+		i++;
+	}
+
+	return (str);
+}
