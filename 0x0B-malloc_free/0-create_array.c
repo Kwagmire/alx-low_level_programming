4b4a901efdebf81a1ed489b58517b8b4bd9de86d
Kwagmire <kwagmire999@gmail.com>
2022-05-05 19:26:02 +0100

    Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -21,15 +21,13 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	str = malloc((size + 1) * sizeof(char));
+	str = malloc(size * sizeof(char));
 
 	if (str == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
 		str[i] = c;
-
-	str[i] = '\0';
 
 	return (str);
 }
