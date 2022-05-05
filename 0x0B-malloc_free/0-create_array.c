51e3312cb555a9baa9ad39c2ba4658d18986ae3d
Kwagmire <kwagmire999@gmail.com>
2022-05-05 18:48:31 +0100

    Float like a butterfly, sting like a bee

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - create an array of chars
+ * and initialize it with a specific char
+ *
+ * @size: size of array
+ * @c: the initialization character
+ *
+ * Return: if successful - a pointer to the array
+ *	   if size == 0 or it fails - NULL
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *str;
+
+	if (size == 0)
+		return (NULL);
+
+	str = malloc(size * sizeof(char) + 1);
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		str[i] = c;
+
+	str[i] = '\0';
+
+	return (str);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+dynamic allocation doesn't look easy
