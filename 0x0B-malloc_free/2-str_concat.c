f0436d0be64368870cfe9028850cb984ccab0329
Kwagmire <kwagmire999@gmail.com>
2022-05-05 21:23:55 +0100

    He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,30 +17,35 @@ char *str_concat(char *s1, char *s2)
 	int i, j, s1_len = 0, total_len;
 
 	if (s1 == NULL)
+	{
 		s1_len += 0;
+	}
+	else
+	{
+		for (i = 0; s1[i]; i++)
+			s1_len++;
+	}
+
+	total_len = s1_len;
+
 	if (s2 == NULL)
 	{
 		total_len = 0 + s1_len;
 	}
 	else
 	{
-		for (i = 0; s1[i]; i++)
-			s1_len++;
-
-		total_len = s1_len;
-
 		for (i = 0; s2[i]; i++)
-		total_len++;
+			total_len++;
 	}
 
 	concat = malloc((total_len + 1) * sizeof(char));
 
 	if (concat == NULL)
 		return (NULL);
+
 	for (i = 0; i < s1_len; i++)
 		concat[i] = s1[i];
-	j = 0;
-	for (; i < total_len; i++)
+	for (j = 0; i < total_len; i++)
 	{
 		concat[i] = s2[j];
 		j++;
