15f450434c2179cf2535d944cc1f5d99dcd530f7
Kwagmire <kwagmire999@gmail.com>
2022-05-12 05:59:30 +0100

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - a dog
+ * @name: its name
+ * @age: its age
+ * @owner: its owner
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
