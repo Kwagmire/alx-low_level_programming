04c00d375700db803729fc7b2da169826586b6ce
Kwagmire <kwagmire999@gmail.com>
2022-08-20 11:06:07 +0100

    Free list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - Free a dlistint_t list
+ *
+ * @head: head of the list
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	if (head != NULL && head->next != NULL)
+	{
+		free_dlistint(head->next);
+		free(head);
+}
